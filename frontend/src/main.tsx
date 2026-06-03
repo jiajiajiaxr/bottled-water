f3b55544f3d9a225af6815d190540f8a860d80c8
Haiyang-Bian <2668798565@qq.com>
2026-06-04 03:10:09 +0800

    refactor(styles): 将全局 CSS 拆分为 SCSS 模块
    - 新增 src/styles/ 目录，按功能拆分为 10 个 SCSS partial
    
    - 提取颜色、间距、圆角等变量到 _variables.scss
    
    - 安装 sass 依赖，利用 SCSS 嵌套语法提升可读性
    
    - 新增 vite-env.d.ts 声明 .scss 模块类型
    
    - 删除旧的 monolithic styles.css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ConfigProvider, theme } from "antd";
 import zhCN from "antd/locale/zh_CN";
 import App from "./App";
-import "./styles.css";
+import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
